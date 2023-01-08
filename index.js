70b19832cb170ab923d030efc6deefca13049d33
LeLuyen <leluyen1205@gmail.com>
2023-01-08 22:00:31 +0700

    handlebars

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
-app.get("/trang-chu1", (req, res) => {
-  var a = 1;
-  var b = 2;
-  var c = a + b;
-  res.send("Hello World!");
+var morgan = require("morgan");
+app.use(morgan("combined"));
+app.get("/trang-chu", (req, res) => {
+  res.send("<h1>Xin chào</h1>");
 });
 
 app.listen(port, () => {
